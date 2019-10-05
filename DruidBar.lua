2c6f54d6d27d7224a7744bc0dc4a3934697a7d32
Bugaloon <54610425+Bugaloon@users.noreply.github.com>
2019-10-05 20:02:58 +1000

    Current Mana Only for "Bliz-Like"
    Added a new function CurrentMana() to work alongside ManaValues() in lieu of completely re-writing how retrieving mana values works for the whole addon, and changed the mana source for the "Bliz-Like" display option to use the new function in place of the old function to better mimic the appearance of the Blizzard UI.

--- DruidBar.lua
+++ DruidBar.lua
@@ -339,7 +339,7 @@ function DruidBar_TextRenderer()
 			dbarShow(DruidBarTextLeft);
 			dbarShow(DruidBarTextRight);
 			DruidBarTextLeft:SetText(ManaPercentage());
-			DruidBarTextRight:SetText(ManaValues());
+			DruidBarTextRight:SetText(CurrentMana());
 			DruidBarTextLeft:SetTextColor(1,1,1,1);
 			DruidBarTextRight:SetTextColor(1,1,1,1);
 			end
@@ -367,6 +367,10 @@ end
 
 function ManaValues()
 	return floor(DruidBarKey.currentmana).."/"..floor(DruidBarKey.maxmana);
+end
+
+function CurrentMana()
+	return floor(DruidBarKey.currentmana);
 end
 
 function ManaPercentage()
